affb39244b2ddc2c6b2baef302c25ab8270c09d3
suifeng412 <suifengin412@gmail.com>
2018-08-28 17:57:25 +0800

    [add] EnumMap测试

--- javase/studyapi/src/mapapi/EnumMapTest.java
+++ javase/studyapi/src/mapapi/EnumMapTest.java
@@ -1,5 +1,7 @@
 package mapapi;
 
+import java.util.EnumMap;
+
 /*
  * EnumMap
  * 
@@ -11,5 +13,17 @@ package mapapi;
  * 
  * */
 public class EnumMapTest {
+	public static void main(String[] args) {
+		//创建一个EnumMap对象，该EnumMap的所有key
+        //必须是Season枚举类的枚举值
+        EnumMap<Season, String> enumMap = new EnumMap<Season, String>(Season.class);
+        enumMap.put(Season.SUMMER , "夏日炎炎");
+        enumMap.put(Season.SPRING , "春暖花开");
+        System.out.println(enumMap);
+	}
+}
 
+enum Season
+{
+    SPRING,SUMMER,FALL,WINTER
 }
